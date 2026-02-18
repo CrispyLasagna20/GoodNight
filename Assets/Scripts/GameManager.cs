763d654e3218e38fa25602b7b688413f8e225f92
CrispyLasagna20 <crispylasagna20@gmail.com>
2026-02-18 18:35:41 -0500

    started Game Manager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class GameManager : MonoBehavior
 {
-    
+    public static GameManager Instance;
+
+    public GameState State;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        State = newState;
+        
+        switch (newState)
+        {
+        }
+    }
+}
+
+public enum GameState
+{
+    TitleScreen,
+    Tutorial,
+    MainGame,
+    LoseScreen,
+    WinScreen
 }
